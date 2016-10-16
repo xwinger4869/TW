16ede3c50eabb8e936b927a43c05e931a0b84780
xwinger4869 <xwinger4869@126.com>
2016-10-16 20:48:57 +0800

    Create SuiteTest.java

---
+++ ThoughtWorks/src/test/SuiteTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({GenerateSummaryTest.class,GetFeeTest.class,
+	GetGroundNumTest.class,GetWeekTest.class})
+
+public class SuiteTest {}
